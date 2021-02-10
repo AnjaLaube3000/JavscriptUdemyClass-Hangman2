276c0a963c25fcbd6f0c70baaf5a2576a608409b
Anja Laube <mail@anjalaube.com>
2021-02-10 12:13:47 +0100

    syntax transformed into Class Syntax

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 const puzzleEl = document.querySelector('#puzzle')
 const guessesEl = document.querySelector('#guesses')
-const game1 = new Hangman('Cat', 2)
+const messageEl = document.querySelector('#message')
+const game1 = new Hangman("playground", 5)
 
 puzzleEl.textContent = game1.getPuzzle()
-guessesEl.textContent = game1.remainingGuesses
-console.log(game1.status)
+messageEl.textContent = game1.statusMessage()
 
 window.addEventListener('keypress', function (e) {
-    const guess = String.fromCharCode(e.charCode)
-    game1.makeGuess(guess)
-    puzzleEl.textContent = game1.getPuzzle()
-    guessesEl.textContent = game1.remainingGuesses
-    console.log(game1.status)
+  const guess = String.fromCharCode(e.charCode)
+  game1.makeGuess(guess)
+  puzzleEl.textContent = game1.getPuzzle()
+  messageEl.textContent = game1.statusMessage()
 })
