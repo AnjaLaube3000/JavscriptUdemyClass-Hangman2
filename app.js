d04bdbd0235e8187a8cf823efcca8289a06d06c6
Anja Laube <mail@anjalaube.com>
2021-02-16 17:37:29 +0100

    http request basic code

--- app.js
+++ app.js
@@ -12,3 +12,39 @@ window.addEventListener('keypress', function (e) {
   puzzleEl.textContent = game1.puzzle
   messageEl.textContent = game1.statusMessage
 })
+
+
+// Making an HTTP Request
+
+// const request = new XMLHttpRequest()
+
+// request.addEventListener('readystatechange', (e) => {
+//   if (e.target.readyState === 4 && e.target.status === 200) {
+//     const data = JSON.parse(e.target.responseText)
+//     console.log(data)
+//   } else if (e.target.readyState === 4) {
+//     console.log('An error has taken place.')
+//   }
+// })
+
+
+// Make a new request for all countries
+// Parse the responseText to get back the array of objects
+// Find your country object by it's country code(alpha2Code property)
+// Print the full country name (name property)
+const countryRequest = new XMLHttpRequest()
+
+const countryCode = 'DE'
+
+countryRequest.addEventListener('readystatechange', (e) => {
+  if (e.target.readyState === 4 && e.target.status === 200) {
+    const data = JSON.parse(e.target.responseText)
+    const match = data.find((match) => match.alpha2Code === countryCode)
+    console.log(match.name)
+  } else if (e.target.readyState === 4) {
+    console.log('An error has taken place.')
+  }
+})
+
+countryRequest.open('GET', 'http://restcountries.eu/rest/v2/all')
+countryRequest.send()
